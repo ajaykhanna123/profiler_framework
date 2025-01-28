b2e2d39166ba73324c7924e34be73b8f1b9d590f
Ajay <ajaykhanna123ak@gmail.com>
2025-01-27 19:26:45 -0600

    added setup

---
+++ build/lib/profiler/tests/test_core.py
@@ -0,0 +1,22 @@
+import pytest
+from profiler.core import Profiler
+import asyncio
+
+def test_profiler():
+    profiler = Profiler()
+
+    @profiler.track_memory
+    def sample_function():
+        a = [i for i in range(1000000)]
+        return sum(a)
+
+    @profiler.track_memory_async
+    async def sample_async_function():
+        await asyncio.sleep(1)
+        return "Done"
+
+    sample_function()
+    asyncio.run(sample_async_function())
+
+    function_data = profiler.get_function_data()
+    assert len(function_data) == 2
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Ajay Khanna',
     author_email='ajaykhanna123ak@gmail.com',
     packages=find_packages(),
+    url="https://github.com/ajaykhanna123/profiler_framework",
     install_requires=[
         'psutil',
         'seaborn',
